29c01a5e317d99fe1d14f5b02fadf68ba1224efe
Vansh Jaiswal <129777298+Vanshjais1777@users.noreply.github.com>
2024-01-12 02:16:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu-container')
+const headerContent = document.querySelector('.header-content')
+const closeIcon = document.querySelector('.close-icon')
+const nav = document.querySelector('nav')
+
+hamburgerIcon.addEventListener('click', (e) => {
+    e.stopPropagation()
+    headerContent.classList.add('menu-open')
+})
+
+nav.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+
+closeIcon.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+window.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+const GoToTop = document.querySelector("body > div")
+
+GoToTop.addEventListener('click', () => {
+    document.querySelector("body > div").scrollTo(0, 0)
+})
